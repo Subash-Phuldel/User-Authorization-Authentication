bd92de10c143a3bbf7830e9eeef0df0c530ac3c8
Subash Phuldel <officalsubashphuldel@gmail.com>
2024-06-30 23:10:30 +0545

    Handling uncaughtExpection and unhandledRejection

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: `${__dirname}/config.env` });
 const mongoose = require("mongoose");
+
+process.on("uncaughtException", function (err) {
+  console.log("UNCAUGHT EXCEPTION , SHUTTING DOWN");
+  console.log(err);
+  process.exit(1);
+});
+
 const app = require("./app");
 
 (async () => {
@@ -14,6 +21,14 @@ const app = require("./app");
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, "127.0.0.1", () => {
+const server = app.listen(port, "127.0.0.1", () => {
   console.log(`Server is listening at port ${port}`);
+});
+
+process.on("unhandledRejection", function (err) {
+  console.log("UNHANDLED REJECTION, SHUTTING DOWN");
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
 });
